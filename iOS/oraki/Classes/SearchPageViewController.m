b72f6e5128105567a0abdadb60a173cf2aaa9bf2
Avi Itskovich <avi@inkling.com>
2011-03-19 14:44:10 -0700

    Add search bar and first page

---
+++ iOS/oraki/Classes/SearchPageViewController.m
@@ -0,0 +1,69 @@
+//
+//  HomePageViewController.m
+//  oraki
+//
+//  Created by Avi Itskovich on 3/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SearchPageViewController.h"
+
+
+@interface SearchPageViewController () <UISearchBarDelegate>
+@end
+
+@implementation SearchPageViewController
+
+@synthesize searchBar = _searchBar;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // j initialization
+
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark -
+#pragma mark UISearchBar Delegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"Search");
+}
+
+#pragma mark - 
+#pragma mark View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
