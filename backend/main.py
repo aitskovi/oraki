0c2d0b75041f9a08431280967e4a4ccb176662de
gilbertl <gilbert.kf.leung@gmail.com>
2011-03-19 19:08:46 -0700

    Handle errors a bit more nicely
    * Lebron James should is redirected to LeBron James
    * articles not found return 404 error
    * if Wiki server down, return 503 error
    * unforseen errors return 500

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,9 @@ class SearchHandler(webapp.RequestHandler):
     """given search query, return suggestions"""
     def get(self):
         query = self.request.get('query', None)
-        if query is None: self.error(400)
+        if query is None:
+            self.error(400)
+            return
         limit = self.request.get('limit', 10)
 
         result = get_json(WIKI_URL,
@@ -48,24 +50,76 @@ class SearchHandler(webapp.RequestHandler):
           })
         if result.status_code != 200:
             self.error(503)
+            return
         json_obj = json.loads(result.content)
         suggestions = json_obj[1]
         self.response.out.write(json.dumps(suggestions))
 
 class ArticleHandler(webapp.RequestHandler):
+
+    def get(self):
+        """given title, return ... sth"""
+        title = self.request.get('title', None)
+        if title is None:
+            self.error(400)
+            return
+
+        # first, get the page id
+        page_info_result = get_json(WIKI_URL,
+            {'action': 'query',
+             'format': 'json',
+             'titles': title,
+             'redirects': ''})
+        if page_info_result.status_code != 200:
+            logging.warning('Wikipedia servers down?')
+            self.error(503)
+            return
+        json_obj = json.loads(page_info_result.content)
+        pages = json_obj['query']['pages']
+        (page_id, page_info) = pages.iteritems().next()
+        if page_id == u'-1':
+            logging.warning('Couldn\'t find title %s', title)
+            self.error(404)
+            return
+
+        # then, get the parsed wiki page
+        parsed_page_result = get_json(WIKI_URL,
+            {'action': 'parse',
+             'format': 'json',
+             'prop': 'text|sections',
+             'pageid': page_id})
+        if parsed_page_result.status_code != 200:
+            logging.warning('Wikipedia servers down?')
+            self.error(503)
+            return
+        page_data = json.loads(parsed_page_result.content)
+        sections_info = [section for section in page_data['parse']['sections'] \
+            if section['toclevel'] == 1]
+        text = page_data['parse']['text']['*']
+        self.response.out.write(json.dumps(
+            [section for section in self.parse_text(text, sections_info) \
+                 if len(section['paragraphs']) > 0]))
+
     def parse_text(self, text, section_info):
-        """return section objects"""
+        """
+        given the wikipedia text in HTML format and a dictinoary of section data
+        returns a list of sectinos (name, paragraphs)
+        """
         soup = BS.BeautifulSoup(text)
         sections = []
         curr_section = {'name': 'Abstract', 'paragraphs': []}
         for iter_elmn in soup.contents:
             if isinstance(iter_elmn, BS.NavigableString):
+                # any strings or comments in the top level can be ignored
                 continue
             elif iter_elmn.name == 'p':
+                # get all the text in the paragraph
                 curr_section['paragraphs'].append(''.join(iter_elmn.findAll(text=True)))
             elif re.match(r'^h[1-5]$', iter_elmn.name):
+                # it's a potential sectino; check if it's in the section_info
                 headline_elmn = iter_elmn.find('span', {'class': 'mw-headline'})
                 if any((headline_elmn.get('id', '') == section['anchor'] for section in section_info)):
+                    # begin a new section
                     sections.append(deepcopy(curr_section))
                     curr_section['name'] = ''.join(headline_elmn.findAll(text=True))
                     curr_section['paragraphs'] = []
@@ -73,35 +127,6 @@ class ArticleHandler(webapp.RequestHandler):
                     continue
         return sections
 
-
-    def get(self):
-        """given title, return ... sth"""
-        title = self.request.get('title', None)
-        if title is None: self.error(400)
-        page_info_result = get_json(WIKI_URL,
-            {'action': 'query',
-             'format': 'json',
-             'titles': title})
-        if page_info_result.status_code != 200:
-            self.error(503)
-        json_obj = json.loads(page_info_result.content)
-        pages = json_obj['query']['pages']
-        (page_id, page_info) = pages.iteritems().next()
-        # TODO what if no pages returned?
-        parsed_page_result = get_json(WIKI_URL,
-            {'action': 'parse',
-             'format': 'json',
-             'prop': 'text|sections',
-             'pageid': page_id})
-        if parsed_page_result.status_code != 200:
-            self.error(503)
-        json_obj = json.loads(parsed_page_result.content)
-        sections_info = [section for section in json_obj['parse']['sections'] \
-            if section['toclevel'] == 1] # TODO: does small pages have toc ?
-        text = json_obj['parse']['text']['*']
-        self.response.out.write(json.dumps(
-            [section for section in self.parse_text(text, sections_info) \
-                 if len(section['paragraphs']) > 0]))
 
 
 def main():
